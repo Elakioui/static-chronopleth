8a49161a955d0e5356f299e1e4458d0dc08e3266
EL AKIOUI Zouhaire <zouhaire.elakiou@gmail.com>
2020-04-13 12:57:33 +0100

    added static and dynamic choropleth

--- static-choropleth-map.py
+++ static-choropleth-map.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
 import plotly as py
-import plotly.express as ex
+import plotly.express as px
 import plotly.graph_objs as go
-
 
 from plotly.subplots import make_subplots
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 init_notebook_mode(connected=True)
 
-#read Data
+#Read Data
 df = pd.read_csv('covid_19_data.csv')
 
 
@@ -24,4 +23,22 @@ df_countries = df.groupby(
 
 df_countries = df_countries.drop_duplicates(subset = ['Country'])
 df_countries = df_countries[df_countries['Confirmed']>0]
-print(df_countries)
+
+# Create static Choropleth
+fig = go.Figure(data=go.Choropleth(
+    locations = df_countries['Country'],
+    locationmode = 'country names',
+    z = df_countries['Confirmed'],
+    colorscale = 'Reds',
+    marker_line_color = 'black',
+    marker_line_width = 0.5,
+))
+fig.update_layout(
+    title_text = 'Confirmed Cases as of March 28, 2020',
+    title_x = 0.5,
+    geo=dict(
+        showframe = False,
+        showcoastlines = False,
+        projection_type = 'equirectangular'
+    )
+)
